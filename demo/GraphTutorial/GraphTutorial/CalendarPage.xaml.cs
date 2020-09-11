2be5485de60fe6dbf4ae6c1972f69815a38abddf
Utkarsh Rathor <uutkarshsingh@gmail.com>
2020-09-11 20:17:36 +0530

    Added the meetings page

--- demo/GraphTutorial/GraphTutorial/CalendarPage.xaml.cs
+++ demo/GraphTutorial/GraphTutorial/CalendarPage.xaml.cs
@@ -23,24 +23,74 @@ namespace GraphTutorial
             InitializeComponent();
         }
 
+        private IUserEventsCollectionPage events;
+        private List<Event> eventsList;
+
         protected override async void OnAppearing()
         {
             base.OnAppearing();
 
             // Get the events
-            var events = await App.GraphClient.Me.Events.Request()
+            events = await App.GraphClient.Me.Events.Request()
                 .Select(e => new 
                 { 
                     e.Subject, 
                     e.Organizer, 
                     e.Start, 
-                    e.End 
+                    e.End ,
+                    e.Attendees,
+                    e.BodyPreview,
                 })
                 .OrderBy("createdDateTime DESC")
                 .GetAsync();
 
             // Add the events to the list view
-            CalendarList.ItemsSource = events.CurrentPage.ToList();
+            eventsList = events.CurrentPage.ToList();
+
+            CalendarList.ItemsSource = eventsList; 
+        }
+
+        void CalendarList_ItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            if (CalendarList.SelectedItem != null)
+            {
+                var selectedEvent = (Event) CalendarList.SelectedItem; 
+
+                var detailPage = new MeetingDetailsPage(selectedEvent);
+
+                Navigation.PushAsync(detailPage);
+            }
+        }
+
+        void DatePicker_DateSelected(System.Object sender, Xamarin.Forms.DateChangedEventArgs e)
+        {
+           var answer =  eventsList.Where((singleEvent) => {
+
+                var startdate = singleEvent.Start.DateTime.Substring(0, 10);
+
+               Console.WriteLine("**->  "+startdate);
+               var actualDate = DateTime.Parse(startdate);
+                
+               var answer1 =DateTime.Compare(actualDate,e.NewDate) == 0;
+
+               Console.WriteLine(actualDate + " " + e.NewDate + " " + answer1);
+
+
+               return answer1;
+
+            });
+
+            foreach (Event eve in answer)
+            {
+                Console.WriteLine(eve.Start);
+            }
+
+            CalendarList.ItemsSource = answer;
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            CalendarList.ItemsSource = eventsList;
         }
     }
 }
---
+++ demo/GraphTutorial/GraphTutorial/MeetingDetailsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Graph;
+using Xamarin.Forms;
+
+namespace GraphTutorial
+{
+    public partial class MeetingDetailsPage : ContentPage
+    {
+        private Event SelectedEvent { get; set; }
+
+        public MeetingDetailsPage(Event selectedEvent)
+        {
+            InitializeComponent();
+            this.meetingName.Text = selectedEvent.Subject;
+            this.startDate.Text = selectedEvent.Start.DateTime;
+            this.listOfAttendees.ItemsSource = selectedEvent.Attendees;
+        }
+
+    }
+}
